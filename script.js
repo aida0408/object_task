2252b9d9fefe2529cb65d79dde53acbd5050ad6f
Bakazova Aida <solnyshko669@gmail.com>
2022-02-11 10:57:22 +0600

    task1

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Напишите функцию, которая принимает массив чисел и возвращает массив этих чисел умноженных на два
-//task1
-let array = [2,3,6]
-let array1 = array.map(elem => elem * 2)
-console.log(array1)
+// function task1(array1,array2){
+//     return array1.map((item, idx) => item + array2[idx])
+// }
+//
+// console.log(task1([2,4,5], [2,4,5]))
+
+// function task1(arr1, arr2){
+//     return [arr1[0] + arr2[0], arr1[1] + arr2[1]]
+// }
+//
+// console.log(task1([1, 1], [1, 1]))
+
+// const task6 = (array) => {
+//     return array.filter((item) => {
+//         return item.length
+//     })
+// }
+
+// function task7 (num){
+//     return num.map(num => num * 2).filter(num => num % 10 === 0)
+// }
+//
+// console.log( task7([1,2,3,7,10,5]))
