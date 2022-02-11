76cfb7b1c6f35838c5984adb5fc51ab383719201
Bakazova Aida <solnyshko669@gmail.com>
2022-02-11 11:57:02 +0600

    task1

--- script.js
+++ script.js
@@ -11,11 +11,49 @@
 //
 // console.log(task1([1, 1], [1, 1]))
 
+//Напишите функцию, которая принимает массив чисел и возвращает массив этих чисел умноженных на два
+
+// function task2(array){
+//     return array.map((item) => item * 2)
+// }
+//
+// console.log(task2([2, 4, 7, 8]))
+
+// Напишите функцию, которая принимает массив объектов с полем id. Например [[{id: '100'}, {id: 2}]] и возвращает массив этих id
+
+// Напишите функцию, которая принимает массив чисел и возвращает массив строк по шаблону <h1>{number}</h1>
+
+// function task4(arr){
+//     return arr.map((item) => `<h1>${item}</h1>`)
+// }
+//
+// console.log(task4([1, 2]))
+// console.log(task4(["Roma"]))
+
+// const task4 = (array) => array.map((item) => `<h1>${item}</h1>`)
+//
+// console.log(task4([1, 2]))
+// console.log(task4(["Roma"]))
+
+// const task4 = (array) => array.map((item, idx) => `<h${idx + 1}>${item}</h${idx + 1}>`)
+// console.log(task4([1,2]))
+// console.log(task4(["Roma", "Dima", "Sasha"]))
+
+// Напишите функцию, которая принимает массив строк и возвращает массив строк с четным количеством букв
+const task5 = (array) => {
+
+}
+console.log(task5([abcd, ads, asdff,ab]))
+console.log(task5([abc, ad, asdf,ab]))
+
+
 // const task6 = (array) => {
 //     return array.filter((item) => {
 //         return item.length
 //     })
 // }
+
+
 
 // function task7 (num){
 //     return num.map(num => num * 2).filter(num => num % 10 === 0)
