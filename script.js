8f599afae77e3ae9ca6db4580c71fbfc6bb6a2a5
Bakazova Aida <solnyshko669@gmail.com>
2022-02-11 20:33:41 +0600

    tasks

--- script.js
+++ script.js
@@ -40,23 +40,54 @@
 // console.log(task4(["Roma", "Dima", "Sasha"]))
 
 // Напишите функцию, которая принимает массив строк и возвращает массив строк с четным количеством букв
-const task5 = (array) => {
-
-}
-console.log(task5([abcd, ads, asdff,ab]))
-console.log(task5([abc, ad, asdf,ab]))
-
-
-// const task6 = (array) => {
+// const task5 = (array) => {
 //     return array.filter((item) => {
-//         return item.length
+//         return item.length % 2 === 0
 //     })
+//
 // }
+// console.log(task5( ['ab','abc','abcd']))
+// console.log(task5(['abc','abcde']))
+
+// Напишите функцию, которая принимает массив чисел и возвращает массив чисел умноженных на два, которые делятся на 10 без остатка
+
+// const task6 = (num) => num.map(num => num * 2).filter(num => num % 10 === 0)
+//
+//
+// console.log(task6([1,2,3,7,10,5]))
 
 
+//Напишите функцию, которая принимает массив булевых значений и возвращает массив только со значениями true
 
-// function task7 (num){
-//     return num.map(num => num * 2).filter(num => num % 10 === 0)
+// function task7(str){
+//     return str.filter((item) => item === true)
+// }
+// console.log(task7( [true,false,true,true]))
+// console.log(task7( [true,false]))
+
+// const task7 = str => str.filter((item) => item === true)
+// console.log(task7( [true,false,true,true]))
+// console.log(task7( [true,false]))
+
+// Напишите функцию, которая принимает массив значений в т.ч и значения undefined и возвращает массив только без значений undefined
+// function task8 (str) {
+//     return str.map((item) => item === true, false)
+//
 // }
 //
-// console.log( task7([1,2,3,7,10,5]))
+// console.log(task8[true, false, true,true, undefined, true, undefined])
+// console.log(task8[undefined, true, undefined])
+
+// let age = 88
+// if (age >= 10 && age <= 20){
+//     console.log(`Mne ${age} let`)
+// }else if (age % 10 === 1){
+//     console.log(`Mne ${age} god`)
+// }else if (age % 10 >= 2 && age % 10 <= 4){
+//     console.log(`Mne ${age} goda`)
+// }else {
+//     console.log(`Mne ${age} let`)
+// }
+// let num = '321'
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2])
+// console.log(sum)
